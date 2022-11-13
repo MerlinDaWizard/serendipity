a10e6c8d03bb01cfd31b41fb5096c8d685929a3a
Merlin <merlinhancock@gmail.com>
2022-11-13 01:16:53 +0000

    hello command test

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,48 @@ use std::env;
 
 use dotenv::dotenv;
 
-use serenity::async_trait;
-use serenity::prelude::*;
-use serenity::model::channel::Message;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, CommandResult};
+use poise::serenity_prelude::{self as serenity, Mention};
 
-#[group]
-#[commands(ping)]
-struct General;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+struct Data{}
 
-struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {}
-
-#[tokio::main]
-async fn main() {
-    dotenv().ok();
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
-        .group(&GENERAL_GROUP);
-
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("Fill in DISCORD_TOKEN at .env");
-    // Use a bitwise OR to add the message context intent, due to intents stored as 53-bit integer bitfield
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
-        .framework(framework)
-        .await
-        .expect("Error creating client");
-
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
+#[poise::command(slash_command, prefix_command)] // This is a function attibute from the poise library
+async fn hello(
+    ctx: Context<'_>, // <'_> means an anonomous lifetime
+    #[description = "Selected user"] #[rest] user: Option<serenity::User>, // Here description is text attached to the command arguement description
+) -> Result<(), Error> { // Returns an empty tuple, meaning success (see ok below) or an error
+    match user {
+        None => {
+            ctx.say("No user mentioned").await?;
+            return Ok(());
+        },
+        Some(user) => {
+            ctx.say(format!("{} says hello to {} :D", Mention::from(ctx.author().id), Mention::from(user.id))).await?;
+            return Ok(());
+        }
     }
 }
 
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
-
+#[poise::command(prefix_command)]
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?; // Buildin command provides a nice button menu
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok(); // Dotenv crate automatically loads environment variables specified in `.env` into the environment
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![hello(), register()], // We specify the commands in an 'array' (vec in rust), we then load the default values for the framework for the rest
+            ..Default::default()
+        })
+        // Login with a bot token from the environment
+        .token(env::var("DISCORD_TOKEN").expect("Fill in DISCORD_TOKEN at .env")) // .expect means we just panic (crash kinda) if its missing
+        // Use a bitwise OR to add the message context intent, due to intents stored as 53-bit integer bitfield
+        .intents(serenity::GatewayIntents::non_privileged())
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {} )}));
+        
+        framework.run().await.unwrap();
 }
